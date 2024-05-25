4f8b0271a8ce85f2bbda093b1d5a8c533ee33bce
Pong Prince <wongtszpong@gmail.com>
2024-05-25 19:59:12 +0800

    scroll in the map page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { PageNotFindComponent } from './page-not-find/page-not-find.component';
-// import { PageNotFindComponent } from './page-not-find/page-not-find.component';
 
-// export class RouteConsts {
-//     public static readonly Home = $localize`:@@home:home`;
-//     public static readonly First = $localize`:@@first:first`;
-//     public static readonly Second = $localize`:@@second:second`;
-//   }
 
 export const routes: Routes = [
     { path: '', title:'Sabah-Home', component: HomeComponent },
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive],
   templateUrl: './second.component.html',
   styleUrl: './second.component.scss'
 })
 export class SecondComponent {
+  constructor(private router: Router) {}
+  @ViewChild('blogHeaderContainerTitleOne') blogHeaderContainerTitleOne!: ElementRef;
+  @ViewChild('blogHeaderContainerTitleTwo') blogHeaderContainerTitleTwo!: ElementRef;
+  @ViewChild('blogHeaderContainerTitleThree') blogHeaderContainerTitleThree!: ElementRef;
+  @ViewChild('blogHeaderContainerTitleFour') blogHeaderContainerTitleFour!: ElementRef;
+
+  navigateToContactPage() {
+  this.router.navigate(['/contact']);
+}
+
+scrollToElement(elementRef: ElementRef) {
 
 }
+
+scrollToOne() {
+  this.blogHeaderContainerTitleOne.nativeElement.scrollIntoView({ behavior: 'smooth' });
+}
+scrollToTwo() {
+  this.blogHeaderContainerTitleTwo.nativeElement.scrollIntoView({ behavior: 'smooth' });
+}
+scrollToThree() {
+  this.blogHeaderContainerTitleThree.nativeElement.scrollIntoView({ behavior: 'smooth' });
+}
+scrollToFour() {
+  this.blogHeaderContainerTitleFour.nativeElement.scrollIntoView({ behavior: 'smooth' });
+}
+}
+
+
